20844d299989bc24f18392b2a31dc1e74dd60972
Salah OSFOR <osfor.salah@gmail.com>
2022-09-24 16:59:03 +0200

    Create test app

--- tguardian/admin.py
+++ tguardian/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tguardian.models import Product
+from tguardian.models import Category
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
--- tguardian/models.py
+++ tguardian/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    description = models.TextField(max_length=2000, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, null=False)
+    description = models.TextField(max_length=200, null=False)
+    price = models.FloatField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
